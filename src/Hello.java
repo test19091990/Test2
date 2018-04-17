430884baa5ceb6003b843a1faf25c64cf176eab1
neelkhutale19 <neelkhutale19@gmail.com>
2018-04-17 00:14:14 -0700

    Conflict 1

--- src/Hello.java
+++ src/Hello.java
@@ -7,11 +7,9 @@ public class Hello {
 		
 		System.out.print("Hello1");
 		System.out.print("Hello2");	
-<<<<<<< HEAD
 	        System.out.print("Hello3");	
-=======
 		System.out.println("Hello4");
->>>>>>> f4d6d222ffdd8fa2d5395433a21394cbe9ac6303
+
 	}
 	
 	
