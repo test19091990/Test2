fd664855398e2be539bf668604c8c1717539d91e
neelkhutale19 <neelkhutale19@gmail.com>
2018-04-17 00:37:46 -0700

    f

--- src/Hello.java
+++ src/Hello.java
@@ -6,17 +6,11 @@ public class Hello {
 		
 		
 		System.out.print("Hello1");
-<<<<<<< HEAD
 		System.out.print("Hello2");	
 	        System.out.print("Hello3");	
 		System.out.println("Hello4");
 
-                System.out.print("Hello5");  
-
-=======
-		
->>>>>>> parent of 7890d10... Second
-		
+                System.out.print("Hello5");  	
 		System.out.println("H4");
 
 	}
